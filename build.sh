26f0619d8dac309991e74b482fde93c049d7c8ae
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2018-05-03 15:17:21 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-docker rmi intrahealth/node-red-docker:0.1.0 || true
-docker build -f Dockerfile -t intrahealth/node-red-docker:0.1.0 .
-docker push intrahealth/node-red-docker:0.1.0
+docker rmi intrahealth/node-red || true
+docker build -f Dockerfile -t intrahealth/node-red .
+docker push intrahealth/node-red-docker
